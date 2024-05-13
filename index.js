dd5edfa66f161b6a9d012a5a909573cf403e1edb
Sharad Singh <143598174+13Sharad@users.noreply.github.com>
2024-05-13 19:36:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+       var typeData = new Typed(".role", {
+           strings: [
+            "First",
+     "Year",
+     "Guide",
+           ],
+           loop:true,
+           typeSpeed:100,
+           backSpeed:80,
+           backDelay:1000,
+       });
+
+       
